a5c89c7f55e068d994ca0202138c457bd168c74c
VInícius <viniciultds2019@gmail.com>
2021-06-25 23:04:39 -0300

    Image slider built with vanilla JS

---
+++ README.md
@@ -0,0 +1,46 @@
+# Code snippet for image slider with vanilla JS
+This is a code snippet for an image slider which can be activated manually by the user but will also automaticaly change images, bouncing between the first one to the last one and then begining from the first one again.
+
+## Changes to be made in case of use
+1. Images must be added inside the element with the `image-slider` class, wheter it's through hard coding or injecting the code with JS. The number of images added will not influence the code, so it won't break after a certain amount of images. Just be sure to add it into the right parent. If the add image is not to be showed first, you must add `style="display: none;`.
+2. If you want to change the used to display the image look for the `.image-slider` class inside the css.
+3. Images are being automatically switched through a `setInterval()` inside an IIFE. If you want to change the timer, modify the timer inside the function.
+4. If you're using this with ES6 Modules or some framework like react, just import the `svg` files into your js files and build your component from there. The CSS will still apply.
+5. Each functionality of the slider in being wrapped into a module for easy visualization.
+
+## Functionalities inside js file
+### `arrowFunc`
+Encapsulates the functionality related to the arrows used to toggle images.
+
+### `btnFunc`
+Encapsulates the functionality related to the footer buttons used to toggle images.
+
+### `timeoutFunc`
+Encapsulates the functionality related to automatically toggle images.
+
+### `myListeners`
+Encapsulates the listeners used by the user to interect with the page.
+
+### IIFE
+Contains the execution of the code.
+
+## Folder strucutre
+
+├── icons
+│   ├── arrow_back.svg
+│   └── arrow_forward.svg
+├── images
+│   ├── nature_five.jpeg
+│   ├── nature_four.jpeg
+│   ├── nature_one.jpg
+│   ├── nature_three.jpeg
+│   └── nature_two.jpg
+├── index.html
+├── README.md
+├── script.js
+├── styles.css
+├── styles.css.map
+└── styles.scss
+
+## Disclaimer
+Sass was used but the same end result can be achieved with just plain CSS.
---
+++ script.js
@@ -0,0 +1,141 @@
+const arrowFunc = (function() {
+
+  const imgArray = Array.from(document.querySelectorAll(".slider-image"))
+
+  const nextImage = () => {
+
+    if (imgArray[imgArray.length-1].style.display === "none") {
+
+      for (i = 0; i <= imgArray.length; i++) {
+
+        if (imgArray[i].style.display != "none") {
+          imgArray[i].style.display = "none";
+          imgArray[i + 1].style.display = "block";
+          break;
+        }
+      }
+
+    } else {
+      return false;
+    }
+  }
+    
+  const previousImage = () => {
+
+    if (imgArray[0].style.display === "none") {
+
+      for (i = 0; i <= imgArray.length; i++) {
+
+        if (imgArray[i].style.display === "block") {
+          imgArray[i].style.display = "none";
+          imgArray[i - 1].style.display = "block";
+          return;
+        }
+      }
+
+    } else {
+      return false;
+    }
+  }
+
+  return {
+    nextImage,
+    previousImage
+  }
+  
+})();
+
+const btnFunc = (() => {
+
+  const imgArray = Array.from(document.querySelectorAll(".slider-image"))
+
+  const chooseImg = (element) => {
+
+    for (i = 0; i < imgArray.length; i++) {
+
+      if (i == element.getAttribute("data-key")) {
+        imgArray[i].style.display = "block"
+
+      } else {
+        imgArray[i].style.display = "none"
+      }
+    }
+  }
+
+  return {
+    chooseImg
+  }
+
+
+})();
+
+const timeoutFunc = (() => {
+
+  const imgArray = Array.from(document.querySelectorAll(".slider-image"))
+
+  const timeOutSetting = () => {
+    for (i = 0; i < imgArray.length; i++) {
+
+      if (imgArray[i].style.display !== "none") {
+
+        if (i === imgArray.length-1) {
+          imgArray[i].style.display = "none"
+          imgArray[0].style.display = "block"
+          break;
+
+        } else {
+          imgArray[i].style.display = "none"
+          imgArray[i+1].style.display = "block"
+          break;
+        }
+      }
+    }
+  }
+
+  return {
+    timeOutSetting
+  }
+
+
+})();
+
+const myListeners =  (function() {
+
+  const nextArrowListener = () => {
+    document.querySelector("#arrow-next").addEventListener('click', () => {
+      arrowFunc.nextImage();
+    })
+  }
+
+  const previousArrowListener = () => {
+    document.querySelector("#arrow-back").addEventListener('click', () => {
+      arrowFunc.previousImage();
+    })
+  }
+
+  const btnListener = () => {
+    for (i = 0; i < document.querySelectorAll(".slider-op").length; i++) {
+      document.querySelectorAll(".slider-op")[i].setAttribute('data-key', i)
+      document.querySelectorAll(".slider-op")[i].addEventListener('click', (e) => {
+        btnFunc.chooseImg(e.target)
+      })
+    }
+  }
+
+  return {
+    nextArrowListener,
+    previousArrowListener,
+    btnListener
+  }
+})();
+
+
+(function () {
+  myListeners.nextArrowListener();
+  myListeners.previousArrowListener();
+  myListeners.btnListener();
+  setInterval(()=>{
+    timeoutFunc.timeOutSetting()
+  }, 10000)
+  
+})();
